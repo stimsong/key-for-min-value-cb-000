f8a1a051873bb2256d229db0860865cbb0d36529
Greg Stimson <stimsong@yahoo.com>
2018-03-20 00:41:37 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do | name, value |
     if low_value == nil || value < low_value
       low_value = value
-      low-name = name
+      low_name = name
     end
   end
   low_name
